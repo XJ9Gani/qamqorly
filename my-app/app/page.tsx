dcb6dfee6f488effae3b06f2ddfb5e0cd6d7bcf5
XJ9Gani <ganisergaliev29@gmail.com>
2026-02-25 22:41:57 +0500

    first commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,17 @@
+import AdviceInfo from "@/widgets/home/advice-info"
+import HeroInfo from "@/widgets/home/hero-info"
+import PracticeInfo from "@/widgets/home/practice-info"
+import WhoWeAre from "@/widgets/home/who-we-are"
+
+const Home = () => {
+  return (
+    <section>
+      <HeroInfo />
+      <PracticeInfo />
+      <AdviceInfo />
+      <WhoWeAre />
+    </section>
+  )
+}
+
+export default Home
